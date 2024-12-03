3f0346bdbb6b8f4ed46c6798b1f22fa5681122c4
Zaradin <crottyjj@hotmail.com>
2024-12-03 15:40:46 +0000

    Integration Lab - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,7 +1,9 @@
 export const getMovies = async () => {
-    const response = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=ead0d8697b6ad0cfd933f243a5eec92d&language=en-US&include_adult=false&page=1`
-    );
+    const response = await fetch("http://localhost:8080/api/movies", {
+        headers: {
+            Authorization: window.localStorage.getItem("token"),
+        },
+    });
     return response.json();
 };
 
