a0272bee83226b9314907c94b7daf046239efe36
Zaradin <crottyjj@hotmail.com>
2024-12-03 15:36:05 +0000

    Integration Lab - Authentication Context and Login Page

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -5,44 +5,52 @@ import PublicPage from "./pages/publicPage";
 import ProfilePage from "./pages/profilePage";
 import MoviesPage from "./pages/moviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
+import LoginPage from "./pages/loginPage";
+import AuthContextProvider from "./contexts/authContext";
+import ProtectedRoutes from "./protectedRoutes";
 import Header from "./components/siteHeader";
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 360000,
-      refetchInterval: 360000,
-      refetchOnWindowFocus: false
+    defaultOptions: {
+        queries: {
+            staleTime: 360000,
+            refetchInterval: 360000,
+            refetchOnWindowFocus: false,
+        },
     },
-  },
 });
 
 const App = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Header/>
-        <ul>
-          <li>
-            <Link to="/">Public</Link>
-          </li>
-          <li>
-            <Link to="/movies">Movies</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path="/" element={ <PublicPage /> } />
-          <Route path="/movies" element={ <MoviesPage /> } />
-          <Route path="/profile" element={ <ProfilePage /> } />
-          <Route path="*" element={ <Navigate to="/" /> } />
-        </Routes>
-    </BrowserRouter>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <AuthContextProvider>
+                    <Header />
+                    <ul>
+                        <li>
+                            <Link to="/">Public</Link>
+                        </li>
+                        <li>
+                            <Link to="/movies">Movies</Link>
+                        </li>
+                        <li>
+                            <Link to="/profile">Profile</Link>
+                        </li>
+                    </ul>
+                    <Routes>
+                        <Route path="/" element={<PublicPage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route element={<ProtectedRoutes />}>
+                            <Route path="/movies" element={<MoviesPage />} />
+                            <Route path="/profile" element={<ProfilePage />} />
+                        </Route>
+                        <Route path="*" element={<Navigate to="/" />} />
+                    </Routes>
+                </AuthContextProvider>
+            </BrowserRouter>
+        </QueryClientProvider>
+    );
 };
 
-const rootElement = createRoot( document.getElementById("root") )
+const rootElement = createRoot(document.getElementById("root"));
 rootElement.render(<App />);
