73792e0b9fe5545f67e69c89d0a7d44324062d08
Zaradin <crottyjj@hotmail.com>
2024-12-03 15:24:16 +0000

    Authentication lab - exercise (genres)

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,7 +1,7 @@
 import movieModel from "./movieModel";
 import asyncHandler from "express-async-handler";
 import express from "express";
-import { getUpcomingMovies } from "../tmdb-api";
+import { getUpcomingMovies, getMovieGenres } from "../tmdb-api";
 
 const router = express.Router();
 
@@ -54,6 +54,14 @@ router.get(
     asyncHandler(async (req, res) => {
         const upcomingMovies = await getUpcomingMovies();
         res.status(200).json(upcomingMovies);
+    })
+);
+
+router.get(
+    "/tmdb/genres",
+    asyncHandler(async (req, res) => {
+        const movieGenres = await getMovieGenres();
+        res.status(200).json(movieGenres);
     })
 );
 
